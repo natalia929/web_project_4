7e5b488a6b302ade2a8fbe969b2e95643d5aabfc
Natalia Wójtowicz <nwojtowicz92@gmail.com>
2021-02-11 18:33:34 +0100

    1st review_fix

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 let editButton = document.querySelector(".profile__edit-button");
+let nameInput = document.querySelector(".form__input_type_name");
+let jobInput = document.querySelector(".form__input_type_job");
+let popup = document.querySelector(".popup");
+let closeIcon = document.querySelector(".popup__close-icon");
+let form = document.querySelector(".form");
+let profileName = document.querySelector(".profile__title");
+let profileJob = document.querySelector(".profile__subtitle");
+let hearts = document.querySelectorAll('.element__heart-icon');
+
 editButton.addEventListener("click", function(){
-    let popup = document.querySelector(".popup");
+    
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
     popup.classList.add("popup_opened");
     
 })
 
-let closeIcon = document.querySelector(".popup__close-icon");
-closeIcon.addEventListener("click", function(){
-    let popup = document.querySelector(".popup");
+function closePopup(){
     popup.classList.remove("popup_opened");
-    
-})
+}
 
-let form = document.querySelector(".form");
+
+closeIcon.addEventListener("click", function(){
+    closePopup();
+})
+    
+
 
 form.addEventListener("submit", function(event){
     
-    let nameInput = document.querySelector(".form__input_type_name");
-    let jobInput = document.querySelector(".form__input_type_job");
-
-    let profileName = document.querySelector(".profile__title");
-    let profileJob = document.querySelector(".profile__subtitle");
-
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
-    let popup = document.querySelector(".popup");
-    popup.classList.remove("popup_opened");
-    
+    closePopup();
+
     event.preventDefault(); 
 })
-
-let hearts = document.querySelectorAll('.element__heart-icon');
 
 
 hearts.forEach(x => x.addEventListener('click', (event) => {
@@ -43,4 +47,5 @@ hearts.forEach(x => x.addEventListener('click', (event) => {
         style.backgroundImage = 'url("images/heart_checked.svg")';
     }
 }));
+
 
