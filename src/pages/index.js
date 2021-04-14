54ee4a3ef6088882beb762a9043773278846d982
Natalia Wójtowicz <nwojtowicz92@gmail.com>
2021-04-15 00:53:25 +0200

    sprint8, 2nd review

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
-import "./styles/index.css";
+import "./pages/index.css";
 
-import {FormValidator} from "../scripts/FormValidator.js";
-import {Card} from "../scripts/Card.js";
-import {Section} from "../scripts/Section.js";
+import {FormValidator} from "../components/FormValidator.js";
+import {Card} from "../components/Card.js";
+import {Section} from "../components/Section.js";
 
 
-import {PopupWithForm} from "../scripts/PopupWithForm.js";
-import {PopupWithImage} from "../scripts/PopupWithImage.js";
+import {PopupWithForm} from "../components/PopupWithForm.js";
+import {PopupWithImage} from "../components/PopupWithImage.js";
 
-import{popupWindow, initialCards, cardListSection, profileName, profileJob} from "../scripts/helpers.js";
+import{popupWindow, initialCards, cardListSection, profileName, profileJob} from "../components/helpers.js";
 
 const editButton = document.querySelector(".profile__edit-button");
 const nameInput = document.querySelector(".form__input_type_name");
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
-    main: './src/index.js'
+    main: './src/pages/index.js'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -52,7 +52,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html" // path to our index.html file
+      template: "./src/pages/index.html" // path to our index.html file
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin() // connect the plugin for merging CSS files
